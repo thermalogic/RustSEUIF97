fa8a646ef2e6c883c2ff8c7844876f60c165168c
thermalogic <cmh@seu.edu.cn>
2023-07-12 13:58:39 +0800

    add examples

--- examples/simple2.rs
+++ examples/simple2.rs
@@ -2,7 +2,7 @@
 //! The simple demo to get all properties 
 #![allow(warnings)]
 fn main() {
-    let prop_map: [(&str, i32);20]=[
+    let prop_map: [(&str, i32);19]=[
         // 1 propertry in IF97 Basic Equation
         ("p  Pressure(MPa)", if97::OP),
         ("t  Temperature(°C)", if97::OT),
@@ -12,7 +12,7 @@ fn main() {
         ("s  Specific entropy(kJ/(kg·K))", if97::OS),
         ("u  Specific internal energy(kJ/kg)", if97::OU),
         ("cp Specific isobaric heat capacity(kJ/(kg·K))", if97::OCP),
-        ("cv Specific isochoric heat capacity(kJ/(kg·K))", if97::OCP),
+        ("cv Specific isochoric heat capacity(kJ/(kg·K))", if97::OCV),
         ("w  Speed of sound(m/s)", if97::OW),
         
         ("x  Steam quality", if97::OX),
@@ -25,13 +25,12 @@ fn main() {
         ("td Thermal diffusivity(um^2/s)", if97::OTD),
         ("pr Prandtl number", if97::OPR),
         ("st Surface tension(mN/m)", if97::OST),
-        ("st Surface tension(mN/m)", if97::OST),
         ("sdc Static Dielectric Constant", if97::OSDC),
 
     ];
     let p:f64 = 3.0;
     let t:f64= 300.0-273.15;
-    for i in 0..20 {
+    for i in 0..19 {
         let value: f64 = if97::pt(p, t, prop_map[i].1);
         println!("\t {} = {:.6}", prop_map[i].0, value);
     }
