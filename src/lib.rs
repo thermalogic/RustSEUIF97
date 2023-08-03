3ace4a0bd1f663f296e49607dd15f822cab2e661
thermalogic <cmh@seu.edu.cn>
2023-08-03 10:50:12 +0800

    reorder

--- src/lib.rs
+++ src/lib.rs
@@ -30,9 +30,9 @@ The following input pairs are implemented:
 ```
 ## Release Notes
 
-* [1.1.4](https://crates.io/crates/if97/1.1.4) - Add the optional parameter of `region` to computer the properties of the specified region quickly
 
-* [1.1.3](https://crates.io/crates/if97/1.1.3) - The multi-step method unleashes the full power of the compiler optimizations while using `powi()` with the `for` loop
+* [1.1.5](https://crates.io/crates/if97/1.1.5) -  The multi-step method unleashes the full power of the compiler optimizations while using `powi()` with the `for` loop
+   * Add the optional parameter of `region` to computer the properties of the specified region quickly
 
 * [1.0.9](https://crates.io/crates/if97/1.0.9) - The shortest addition chain computes integer powers of a number to provide the stable speed performance under the various hardware and software platforms
 
