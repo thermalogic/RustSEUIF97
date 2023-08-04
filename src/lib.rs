86045151f716dd632917b0a5411f91db5786d9b0
thermalogic <cmh@seu.edu.cn>
2023-08-05 00:27:51 +0800

    speed-up (p.T)->v 3

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,7 @@ The following input pairs are implemented:
 ## Release Notes
 
 
-* [1.1.7](https://crates.io/crates/if97/1.1.7) -  The multi-step method unleashes the full power of the compiler optimizations while using `powi()` with the `for` loop
+* [1.1.9](https://crates.io/crates/if97/1.1.9) -  The multi-step method unleashes the full power of the compiler optimizations while using `powi()` with the `for` loop
    * Add the optional parameter of `region` to computer the properties of the specified region quickly
 
 * [1.0.9](https://crates.io/crates/if97/1.0.9) - The shortest addition chain computes integer powers of a number to provide the stable speed performance under the various hardware and software platforms
