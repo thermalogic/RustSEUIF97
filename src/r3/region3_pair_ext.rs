dd1cc340f5dfebc97002782b106a3f00c51de693
thermalogic <cmh@seu.edu.cn>
2023-08-04 21:44:12 +0800

    doc

--- src/r3/region3_pair_ext.rs
+++ src/r3/region3_pair_ext.rs
@@ -1,4 +1,4 @@
-//！IAPWS-IF97 Region3: The extended input pair
+//！IAPWS-IF97 Region3: The extended input pairs
 //! *  (p,v)
 //! *  (T,h),(T,s)
 
@@ -7,7 +7,7 @@ use crate::common::boundaries::*;
 use crate::common::constant::*;
 use crate::r3::*;
 
-///   (p,v) ->T
+/// Region 3: (p,v) ->T
 pub fn pv2T_reg3(p: f64, v: f64) -> f64 {
     let mut T1: f64 = T_MIN3;
     let mut T2: f64 = B23_p2T(p);
@@ -17,7 +17,7 @@ pub fn pv2T_reg3(p: f64, v: f64) -> f64 {
     rtsec1(Td2p_reg3, d, p, T1, T2, f1, f2, ESP, I_MAX)
 }
 
-///     (T,h) ->d
+///  Region 3: (T,h) ->d
 pub fn Th2d_reg3(T: f64, h: f64) -> f64 {
     let p1: f64 = B23_T2p(T);
     let d1: f64 = 1.0 / pT2v_reg3(p1, T);
@@ -28,7 +28,7 @@ pub fn Th2d_reg3(T: f64, h: f64) -> f64 {
     rtsec2(Td2h_reg3, T, h, d1, d2, f1, f2, ESP, I_MAX)
 }
 
-// Region 3  (T,s)->d using the secant method
+/// Region 3  (T,s)->d using the secant method
 ///  * T: temperature  K
 ///  * s: specific entropy  kJ/(kg K)
 ///  * d: density    kg/m^3
