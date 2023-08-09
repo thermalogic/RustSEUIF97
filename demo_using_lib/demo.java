7a1de55490cc7e2b39750ac6400f298483d21df2
thermalogic <cmh@seu.edu.cn>
2023-08-09 16:07:16 +0800

    add Fortran example

--- demo_using_lib/demo.java
+++ demo_using_lib/demo.java
@@ -1,3 +1,4 @@
+
 /*
    The Java example with Java Native Access(JNA) to call the if87 shared library
 
@@ -11,20 +12,21 @@
 import com.sun.jna.Native;
 import com.sun.jna.Library;
 
-interface if97 extends Library { 
-  
-    if97 lib = (if97)Native.load("../target/release/if97",  if97.class);
-    public double pt(double p, double t, int o_id);  
-   
+interface if97 extends Library {
+
+    if97 lib = (if97) Native.load("../target/release/if97", if97.class);
+
+    public double pt(double p, double t, int o_id);
+
 }
 
 public class demo {
-    
-    public static void main(String[] args){
-        double p=16.0;
-        double t=540.0;
+
+    public static void main(String[] args) {
+        double p = 16.0;
+        double t = 540.0;
         double h;
-        h=if97.lib.pt(p,t,4);
-        System.out.printf("(p,t)->h: (%.1f %.1f) h: %.2f\n",p,t,h);
+        h = if97.lib.pt(p, t, 4);
+        System.out.printf("(p,t)->h: (%.1f %.1f) h: %.2f\n", p, t, h);
     }
 }
