12673ab2965b291b6a60201e0f9f6f010be073d7
thermalogic <cmh@seu.edu.cn>
2023-08-13 19:31:13 +0800

    seuif97 in Rust

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from setuptools_rust import RustExtension
 setup(
     packages=find_packages(where="python"),
     package_dir={"": "python"},
-    rust_extensions=[RustExtension("seuif97.seuif97")],
+    rust_extensions=[RustExtension("seuif97.seuif97",
+                                   features=["python"],)],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering",
