8fe077110d57d822544eaf5bd3460f5e77bad22b
thermalogic <cmh@seu.edu.cn>
2025-03-16 15:33:00 +0800

    go

---
+++ demo_using_lib/demo.go
@@ -0,0 +1,15 @@
+package main
+/*
+#cgo LDFLAGS: -LC:/Windows/system -llibseuif97
+double pt(double p, double t, int o_id);
+*/
+import "C"
+import "fmt"
+
+func main() {
+	p:=16.14
+	t:=512.4
+	oid:=4
+	h := C.pt(C.double(p),C.double(t),C.int(oid))
+	fmt.Printf("(p,t)=(%.2f, %.2f), h=%.2f\n",p,t,float64(h))
+}
