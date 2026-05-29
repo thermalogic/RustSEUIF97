b0cff8f3a327ff29da9fedac325ecc5550a9702a
thermalogic <cmh@seu.edu.cn>
2026-05-29 23:57:32 +0800

    example

--- demo_using_lib/demo_pypi.py
+++ demo_using_lib/demo_pypi.py
@@ -5,16 +5,20 @@ Author:   Cheng Maohua
 Email:    cmh@seu.edu.cn
 
 """
-from seuif97 import pt, pt2s
+from seuif97 import pt, pt2h,pt2s
 
 OH = 4
+OS = 5
 
 p = 16.0
 t = 535.1
 
 # ??(in1,in2,o_id)
 h = pt(p, t, OH)
+s = pt(p, t, OS)
+print(f"??(in1,in2,o_id): p={p}, t={t} h={h:.3f} s={s:.3f}")
 
 # ??2?(in1,in2)
+h = pt2h(p,t)
 s = pt2s(p, t)
-print(f"p={p}, t={t} h={h:.3f} s={s:.3f}")
+print(f"   ??2?(in1,in2): p={p}, t={t} h={h:.3f} s={s:.3f}")
