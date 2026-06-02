57ca03b959bd0210fecd6cd3e936fecdb7f496c1
thermalogic <cmh@seu.edu.cn>
2026-06-02 15:15:29 +0800

    bench

--- benches/speed_benchmark.rs
+++ benches/speed_benchmark.rs
@@ -11,9 +11,11 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     c.bench_function("tv2h_reg3", |b| b.iter(|| tv(black_box(650.0 - 273.15), black_box(1.0 / 500.0), black_box(OH))));
     c.bench_function("tv2s_reg3", |b| b.iter(|| tv(black_box(650.0 - 273.15), black_box(1.0 / 500.0), black_box(OS))));
-
-    c.bench_function("pT2h_reg5", |b| b.iter(|| pt(black_box(0.5), black_box(1500.0 - 273.15), black_box(OH))));
-    c.bench_function("pT2s_reg5", |b| b.iter(|| pt(black_box(0.5), black_box(1500.0 - 273.15), black_box(OS))));
+    c.bench_function("pt2h_reg3", |b| b.iter(|| pt(black_box(50.0), black_box(630.0-273.15), black_box(OH))));
+    c.bench_function("pt2s_reg3", |b| b.iter(|| pt(black_box(50.0), black_box(630.0-273.15), black_box(OS))));
+  
+    c.bench_function("pt2h_reg5", |b| b.iter(|| pt(black_box(0.5), black_box(1500.0 - 273.15), black_box(OH))));
+    c.bench_function("pt2s_reg5", |b| b.iter(|| pt(black_box(0.5), black_box(1500.0 - 273.15), black_box(OS))));
 }
 
 criterion_group!(benches, criterion_benchmark);
