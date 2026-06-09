03c3d6828c32cb8def340db1031d3e8c7cce4f06
thermalogic <cmh@seu.edu.cn>
2026-06-10 00:34:53 +0800

    bench

--- benches/speed_benchmark.rs
+++ benches/speed_benchmark.rs
@@ -1,6 +1,6 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
-
+use criterion::{criterion_group, criterion_main, Criterion, Throughput};
 use seuif97::*;
+use std::hint::black_box;
 use std::time::Duration;
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -11,19 +11,19 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     group.throughput(Throughput::Elements(1));
     
-   // group.bench_function("pt2h_reg1", |b| b.iter(|| pt(black_box(3.0), black_box(300.0 - 273.15), black_box(OH))));
-   // group.bench_function("pt2s_reg1", |b| b.iter(|| pt(black_box(3.0), black_box(300.0 - 273.15), black_box(OS))));
+    group.bench_function("pt2h_reg1", |b| b.iter(|| pt(black_box(3.0), black_box(300.0 - 273.15), OH)));
+    group.bench_function("pt2s_reg1", |b| b.iter(|| pt(black_box(3.0), black_box(300.0 - 273.15), OS)));
     
-   // group.bench_function("pt2h_reg2", |b| b.iter(|| pt(black_box(0.0035), black_box(300.0 - 273.15), black_box(OH))));
-   // group.bench_function("pt2s_reg2", |b| b.iter(|| pt(black_box(0.0035), black_box(300.0 - 273.15), black_box(OS))));
+    group.bench_function("pt2h_reg2", |b| b.iter(|| pt(black_box(0.0035), black_box(300.0 - 273.15), OH)));
+    group.bench_function("pt2s_reg2", |b| b.iter(|| pt(black_box(0.0035), black_box(300.0 - 273.15), OS)));
 
-   // group.bench_function("tv2h_reg3", |b| b.iter(|| tv(black_box(650.0 - 273.15), black_box(1.0 / 500.0), black_box(OH))));
-   // group.bench_function("tv2s_reg3", |b| b.iter(|| tv(black_box(650.0 - 273.15), black_box(1.0 / 500.0), black_box(OS))));
-   // group.bench_function("pt2h_reg3", |b| b.iter(|| pt(black_box(50.0), black_box(630.0-273.15), black_box(OH))));
-   // group.bench_function("pt2s_reg3", |b| b.iter(|| pt(black_box(50.0), black_box(630.0-273.15), black_box(OS))));
+    group.bench_function("tv2h_reg3", |b| b.iter(|| tv(black_box(650.0 - 273.15), black_box(1.0 / 500.0), OH)));
+    group.bench_function("tv2s_reg3", |b| b.iter(|| tv(black_box(650.0 - 273.15), black_box(1.0 / 500.0), OS)));
+    group.bench_function("pt2h_reg3", |b| b.iter(|| pt(black_box(50.0), black_box(630.0-273.15), OH)));
+    group.bench_function("pt2s_reg3", |b| b.iter(|| pt(black_box(50.0), black_box(630.0-273.15), OS)));
 
-    group.bench_function("pt2h_reg5", |b| b.iter(|| pt(black_box(0.5), black_box(1500.0 - 273.15), black_box(OH))));
-   // group.bench_function("pt2s_reg5", |b| b.iter(|| pt(black_box(0.5), black_box(1500.0-273.15), black_box(OS))));
+    group.bench_function("pt2h_reg5", |b| b.iter(|| pt(black_box(0.5), black_box(1500.0 - 273.15), OH)));
+    group.bench_function("pt2s_reg5", |b| b.iter(|| pt(black_box(0.5), black_box(1500.0-273.15), OS)));
     
     group.finish();
 }
