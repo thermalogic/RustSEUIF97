10090762573451077b8139b994502c769a375f2a
thermalogic <cmh@seu.edu.cn>
2026-06-14 03:48:27 +0800

    opti  reg(h,s)

--- examples/simple.rs
+++ examples/simple.rs
@@ -19,4 +19,37 @@ fn main() {
     // Direct Property Functions
     let mut v = pt2v(p, t);
     println!("pt: p={p:.6} t={t:.6} h={h:.6} s={s:.6} v={v:.6}");   
+    
+    let P_MIN: f64 = 0.000611212677444;
+    let s_r5_1=pt(50.0, 1073.15-275.15,(OS,5));
+    let s_r5_2=pt(P_MIN, 2273.15-273.15,(OS,5));
+    let h_r5_1=pt(50.0, 1073.15-273.15,(OH,5)); 
+    let h_r5_2= pt(P_MIN, 2273.15-273.15,(OH,5));
+    println!(" s_r5_1={s_r5_1:.8} s_r5_2={s_r5_2:.8} h_r5_1={h_r5_1:.8} h_r5_2={h_r5_2:.8}");   
+
+    let s_min= pt(100.0, 273.15-273.15,(OS,1));
+    let s_max= pt(P_MIN, 1073.15-273.15,(OS,2));
+    println!(" s_min={s_min:.8} s_max={s_max:.8}");   
+
+     let s13: f64 = pt(100.0, 623.15-273.15,(OS,1));
+     println!(" s13={s13:.8}");   
+     let Ps_623: f64 = 16.5291642526045;
+     let s13s: f64 = pt(Ps_623, 623.15-273.15,(OS,1));
+     println!(" s13s={s13s:.8}");   
+     let h23min: f64 = pt(Ps_623, 623.15-273.15,(OH,2));
+     println!(" h23min={h23min:.8}");  
+     let h23max: f64 = pt(100.0, 863.15-273.15,(OH,2)); 
+     println!(" h23max={h23max:.8}");  
+     let sTPmax: f64 = pt(100.0, 1073.15-273.15,(OS,2));
+     println!(" sTPmax={sTPmax:.8}");  
+     let s2ab: f64 = pt(4.0, 1073.15-273.15,(OS,2)); 
+     println!(" s2ab={s2ab:.8}"); 
+     let hmin: f64 = pt(P_MIN, 273.15-273.15,(OH,2)); 
+     println!(" hmin={hmin:.8}");  
+
+    let h4l: f64 = pt(P_MIN, 273.15-273.15,(OH,1));
+    let s4l: f64 = pt(P_MIN, 273.15-273.15,(OS,1));
+    let h4v: f64 = pt(P_MIN, 273.15-273.15,(OH,2));
+    let s4v: f64 = pt(P_MIN, 273.15-273.15,(OS,2));
+    println!(" h4l={h4l:.8} s4l={s4l:.8} h4v={h4v:.8} s4v={s4v:.8}");  
 }
